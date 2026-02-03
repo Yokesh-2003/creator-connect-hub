eccbcccb9440ba96194654f8d0448b876ef4ec98
yokeshhacwhaba <yokeshhacwhaba@gmail.com>
2026-02-03 06:32:53 +0000

    Fix: Switch to HashRouter for Vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
